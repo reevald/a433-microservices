e10f44d5d7698897eb2ece0dbdd322259a0ebdc9
reevald <mochammadgalangrivaldo@gmail.com>
2023-03-16 21:54:34 +0700

    Add script to build and push image

---
+++ build_push_image_karsajobs_ui.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+# 1) Membuild Docker image dari berkas Dockerfile
+docker build -f Dockerfile -t ghcr.io/reevald/karsajobs-ui:latest .
+# 2) Login ke GitHub Packages
+echo $CR_PAT | docker login ghcr.io -u reevald --password-stdin
+# 3) Push image ke GitHub Packages
+docker push ghcr.io/reevald/karsajobs-ui:latest
